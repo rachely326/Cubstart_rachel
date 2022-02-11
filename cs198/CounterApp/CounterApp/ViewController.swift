bec8099bc7e0262bc8d1685019a3d5f2ef420be4
杨诗月 <yangshiyue@yangshiyuedeMacBook-Air.local>
2022-02-10 20:05:51 -0800

    final submission

---
+++ cs198/CounterApp/CounterApp/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CounterApp
+//
+//  Created by 杨诗月 on 10/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBOutlet weak var count: UILabel!
+}
+
+//var countNum = 0
